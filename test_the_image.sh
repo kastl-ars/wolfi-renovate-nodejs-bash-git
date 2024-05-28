1edda965b607c104b3069512c02687a8001bbd2a
Johannes Kastl <johannes.kastl@ars.de>
2024-05-28 14:48:13 +0200

    test_the_image.sh: also test latest tag

--- test_the_image.sh
+++ test_the_image.sh
@@ -4,3 +4,8 @@ podman run \
     --rm \
     --pull=always \
     ghcr.io/kastl-ars/wolfi-renovate-nodejs-bash-git:22 --version
+
+podman run \
+    --rm \
+    --pull=always \
+    ghcr.io/kastl-ars/wolfi-renovate-nodejs-bash-git:latest --version
